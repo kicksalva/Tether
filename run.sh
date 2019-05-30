6e80cc1e7b55d64bec6112eab2affcb37b5a79e4
Karl Salva <ksalva@localhost.localdomain>
2019-05-30 00:43:28 +0000

    update run script to create routes and monitor connectivity.

--- run.sh
+++ run.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# run linux tether client
+sudo pkill -f "linux/run.sh"
+sudo linux/run.sh &> run_log.txt &
+
+# wait for tether app to start
+until grep "Checking phone status" run_log.txt; do
+    sleep 0.1
+done
+
+# setup routes
+./route.sh
+
 # run internet connectivity monitor in the background
 pkill -f "linux/check_connectivity.sh"
-linux/check_connectivity.sh &> connectivity.txt &
+linux/check_connectivity.sh &> connectivity_log.txt &
 
-# run linux tether client
-sudo linux/run.sh
+
