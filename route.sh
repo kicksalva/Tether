1af070ed2bcba92dbd04919fcf16e54d4cc5884d
Karl Salva <kicksalva@gmail.com>
2019-05-30 01:49:47 +0100

    move interface to variable defined at top of route.sh

--- route.sh
+++ route.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo ifconfig ens33 192.168.17.2 netmask 255.255.255.0 up 
+interface=eth0
+#interface=ens33
+
+sudo ifconfig $interface 192.168.17.2 netmask 255.255.255.0 up 
 
 sudo route add default gw 10.0.0.1
 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -I FORWARD -i ens33 -o tun -s 192.168.17.0/24 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -I FORWARD -i $interface -o tun -s 192.168.17.0/24 -m conntrack --ctstate NEW -j ACCEPT
 sudo iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -t nat -I POSTROUTING -o tun -j MASQUERADE
 
